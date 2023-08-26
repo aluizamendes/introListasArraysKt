6f2cddb1673c1856460015fc7ab74778ebd87858
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-08-26 13:28:21 -0300

    aula 01

---
+++ src/main/kotlin/Arrays.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val idades: IntArray = intArrayOf(25, 19, 33, 10, 55) // array de inteiros
+
+    var maiorIdade: Int = 0
+    for (idade in idades) {
+        if (idade > maiorIdade) {
+            maiorIdade = idade
+        }
+    }
+    var menorIdade = Int.MAX_VALUE // constante que possui maior valor de um inteiro
+    idades.forEach { idade -> if (idade < menorIdade) menorIdade = idade }
+
+    println("A maior idade na lista é $maiorIdade")
+    println("A menor idade na lista é $menorIdade")
+}
---
+++ src/main/kotlin/Salarios.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
+
+    val aumento: Double = 1.1
+    var indice: Int = 0
+
+    for (salario in salarios) {
+        salarios[indice] *= aumento
+        indice++
+    }
+
+    println(salarios.contentToString())
+
+    // obtem o indice do elemento atual e o elemento
+    salarios.forEachIndexed { i, salario -> salarios[i] = salario * aumento }
+
+    println(salarios.contentToString())
+}
